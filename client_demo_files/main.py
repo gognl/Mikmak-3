a5f716d037ad59f50d7c0155da1d1b0c58b5052e
gognl <96839539+gognl@users.noreply.github.com>
2023-01-02 12:42:56 +0200

    bla bla bla shta'im
    idk alon told me to do this this does not reflect my ideological beliefs nor my non-ideological ones

--- client_demo_files/main.py
+++ client_demo_files/main.py
@@ -1,9 +1,38 @@
 import socket
 import pygame
+from client_demo_files.settings import *
+
+
+def initialize_game() -> (pygame.Surface, pygame.time.Clock):
+	"""
+	    Initializes the game.
+	    :return: screen, clock
+	"""
+	pygame.init()
+	size = (WINDOW_WIDTH, WINDOW_HEIGHT)
+	screen = pygame.display.set_mode(size)
+	pygame.display.set_caption("Cows")
+	clock = pygame.time.Clock()
+	return screen, clock
+
+
+def run_game():
+
+	running = True
+	while running:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+
+
+def close_game():
+	pygame.quit()
 
 
 def main():
-	pass
+	initialize_game()
+	run_game()
+	close_game()
 
 
 if __name__ == '__main__':
