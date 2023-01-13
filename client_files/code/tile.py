4903a5bdb2cbb4c74e1e7bfb9f29f80e562f2ee6
bouncingflopy <omrif06@gmail.com>
2023-01-14 00:39:43 +0200

    Map
    Video up to 1:56:00
    Well designed map please like
    Made with Tiled

--- client_files/code/tile.py
+++ client_files/code/tile.py
@@ -3,14 +3,19 @@ from client_files.code.settings import *
 
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, pos, groups) -> None:
+    def __init__(self, pos, groups, sprite_type, surface=pygame.Surface((TILESIZE, TILESIZE))) -> None:
         super().__init__(groups)
 
+        self.sprite_type = sprite_type
+
         # Load image from files
-        self.image: pygame.Surface = pygame.image.load('../graphics/rock.png').convert_alpha()
+        self.image: pygame.Surface = surface
 
         # Position of the tile
         self.rect: pygame.Rect = self.image.get_rect(topleft=pos)
 
         # Tile hitbox - shrink the original hitbox in the vertical axis for tile overlap
-        self.hitbox = self.rect.inflate(0, -10)
+        if self.sprite_type != "barrier":  # Barrier tiles don't get inflated collision boxes
+            self.hitbox = self.rect.inflate(0, -10)
+        else:
+            self.hitbox = self.rect.inflate(0, 26)  # Undo player collision box inflation
