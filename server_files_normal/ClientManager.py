16619c8bcc35c9673efeed5a208d53da74d9a471
gognl <96839539+gognl@users.noreply.github.com>
2023-01-21 11:04:44 +0200

    minor updates

--- server_files_normal/ClientManager.py
+++ server_files_normal/ClientManager.py
@@ -3,7 +3,7 @@ import threading
 from collections import deque
 from typing import Tuple, Union
 
-from server_files_normal.structures import ClientInputMsg, GameState
+from server_files_normal.structures import ClientInputMsg, StateUpdateMsg, ServerSwitchMsg
 
 
 class ClientManager(threading.Thread):
@@ -24,8 +24,26 @@ class ClientManager(threading.Thread):
         """
 
         while True:
-            data = self.client_sock.recv(1024)
+            data: bytes = self._receive_pkt()
             self.queue.append((self, ClientInputMsg(ser=data)))
+
+    def _receive_pkt(self) -> bytes:
+        """Receives and decrypts a message from the client"""
+        data = self.client_sock.recv(1024)
+        # decrypt here
+        return data
+
+    def _send_pkt(self, pkt: bytes):
+        """Encrypts and then sends a packet to the client"""
+        # encrypt here
+        self.client_sock.send(pkt)
+
+    def send_msg(self, msg: Union[StateUpdateMsg, ServerSwitchMsg]):
+        data: bytes = msg.serialize()
+        self._send_pkt(data)
+
+    def has_messages(self):
+        return len(self.queue) != 0
 
     def get_new_message(self) -> Union[Tuple['ClientManager', ClientInputMsg], None]:
         if len(self.queue) == 0:
--- server_files_normal/GameManager.py
+++ server_files_normal/GameManager.py
@@ -12,20 +12,23 @@ class GameManager(threading.Thread):
 	def __init__(self, client_managers: deque):
 		super().__init__()
 		self.client_managers: deque[ClientManager] = client_managers
-		self.queue: deque[Tuple[ClientManager, ClientInputMsg]] = deque()
+		self.client_msgs_queue: deque[Tuple[ClientManager, ClientInputMsg]] = deque()
 		threading.Thread(target=self.add_messages_to_queue).start()
 
 	def add_messages_to_queue(self):
 		while True:
 			for client_manager in self.client_managers:
-				self.queue.append(client_manager.get_new_message())
+				if client_manager.has_messages():
+					self.client_msgs_queue.append(client_manager.get_new_message())
 
 	def run(self):
 		while True:
-			if len(self.queue) == 0:
+			if len(self.client_msgs_queue) == 0:
 				continue
 
-			client_msg = self.queue.pop()
+			client_manager: ClientManager
+			client_msg: ClientInputMsg
+			client_manager, client_msg = self.client_msgs_queue.pop()
 
 
 			# TODO: deal with this message
