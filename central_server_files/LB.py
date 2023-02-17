ef7c22196530cf31b6d53b8580f09047154c7b43
AlonRos <alon.rosenperl@gmail.com>
2023-02-17 18:45:09 +0200

    send players in overlapped areas and update players dict

--- central_server_files/LB.py
+++ central_server_files/LB.py
@@ -7,7 +7,7 @@ from collections import deque
 import pygame
 from structures import *
 from encryption import *
-
+import server_files_normal.game as game
 
 def rand(n):
 	return random.randrange(n)
@@ -21,10 +21,13 @@ H = 600  # 30720
 normal_servers = [Server("0.0.0.0", i) for i in range(amount_servers)]
 center = Point(W//2, H//2)
 players = {player_id: PlayerCentral(Point(rand(W), rand(H)), player_id) for player_id in range(100)}
+players_overlap = {}
+overlapping_area_T = 10
 
+def send(sock: socket.socket, server: Server, obj):
+	sock.sendto("{obg}".encode(), server.addr())  # TODO: send the serializable form of obj
 
 def get_new_center(players: dict[ID, PlayerCentral]):
-
 	if len(players) == 0:
 		return Point(W//2, H//2)
 
@@ -32,9 +35,11 @@ def get_new_center(players: dict[ID, PlayerCentral]):
 	for key in players:
 		avg.add(players[key].pos)
 	avg.div(len(players))
-	return avg
 
-def find_suitable_server_index(player: PlayerCentral, center: Point) -> int:
+	global center
+	center = avg
+
+def find_suitable_server_index(player: PlayerCentral) -> int:
 	b0 = player.pos.x > center.x
 	b1 = player.pos.y > center.y
 	return 2*b1+b0
@@ -44,7 +49,7 @@ def look_for_new_client(new_clients_q: deque[PlayerCentral], msgs_to_clients_q: 
 		if len(new_clients_q) == 0:
 			continue
 		new_client = new_clients_q.pop()
-		suitable_server = servers[find_suitable_server_index(new_client, center)]
+		suitable_server = servers[find_suitable_server_index(new_client)]
 		msg = MsgToClient(new_client.id,
 		                  b'{suitable_server}')  # TODO: change parameter to the serializable encoding of suitable_server
 		msgs_to_clients_q.append(msg)
@@ -63,7 +68,32 @@ def get_msgs_from_socket(sock: socket.socket, servers: list[Server]):
 		if server is None:
 			continue
 		decrypt(data, server.key)
-		
+
+		if data[0] == '\x00':
+			player = PlayerCentral(data[1:])  # TODO: Do it with serializable
+			players[player.id] = player
+
+		elif data[0] == '\x01':
+			player = game.player.Player(data[1:])  # TODO: Do it with serializable
+			players_overlap[player.entity_id] = player
+
+		elif data[0] == '\x02':
+			player_id = data[1:]
+			players_overlap.pop(player_id)
+
+def send_overlapped_players_details(sock: socket.socket, servers: list[Server]):
+	for player in players_overlap:
+		if player.pos in Rect(0, 0, center.x+overlapping_area_T, center.y+overlapping_area_T):
+			send(sock, servers[0], player)
+
+		if player.pos in Rect(center.x-overlapping_area_T, 0, W, center.y-overlapping_area_T):
+			send(sock, servers[1], player)
+
+		if player.pos in Rect(0, center.x-overlapping_area_T, center.x+overlapping_area_T, H):
+			send(sock, servers[2], player)
+
+		if player.pos in Rect(center.x-overlapping_area_T, center.y-overlapping_area_T, W, H):
+			send(sock, servers[3], player)
 
 
 def LB_main(new_players_q: deque[PlayerCentral], msgs_to_clients_q: deque[MsgToClient]):
@@ -75,8 +105,14 @@ def LB_main(new_players_q: deque[PlayerCentral], msgs_to_clients_q: deque[MsgToC
 	look_for_new_client_Thread = threading.Thread(target=look_for_new_client, args=(new_players_q, msgs_to_clients_q))
 	threads.append(look_for_new_client_Thread)
 
-	update_center_Thread = threading.Thread(target=get_new_center, args=(players,))
-	threads.append(update_center_Thread)
+	send_overlapped_players_details_Thread = threading.Thread(target=send_overlapped_players_details, args=(sock, normal_servers))
+	threads.append(send_overlapped_players_details_Thread)
+
+	get_msgs_from_socket_Thread = threading.Thread(target=get_msgs_from_socket, args=(sock, normal_servers))
+	threads.append(get_msgs_from_socket_Thread)
+
+	# update_center_Thread = threading.Thread(target=get_new_center, args=(players,))
+	# threads.append(update_center_Thread)
 
 	for thread in threads:
 		thread.start()
--- central_server_files/structures.py
+++ central_server_files/structures.py
@@ -40,3 +40,17 @@ class Server:
 		self.ip: str = ip
 		self.port: int = port
 		self.key: bytes = bytes(0)
+
+	def addr(self):
+		return self.ip, self.port
+
+class Rect:
+	def __init__(self, x1: int, y1: int, x2: int, y2: int):
+		self.x1: int = x1
+		self.y1: int = y1
+		self.x2: int = x2
+		self.y2: int = y2
+
+	def __contains__(self, item):
+		assert isinstance(item, Point)
+		return self.x1 <= item.x <= self.x2 and self.y1 <= item.y <= self.y2
