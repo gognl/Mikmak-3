6142e6801536ecc559c2c49556f2a38c49118e06
AlonRos <alon.rosenperl@gmail.com>
2023-03-08 17:45:46 +0200

    nuuu

--- server_files_normal/encryption.py
+++ server_files_normal/encryption.py
@@ -1,6 +1,7 @@
 from cryptography.fernet import Fernet
 from central_server_files.Constant import SALT
 import hashlib
+
 def encrypt(msg: bytes, key: bytes) -> bytes:
     return Fernet(key).encrypt(msg)
 
